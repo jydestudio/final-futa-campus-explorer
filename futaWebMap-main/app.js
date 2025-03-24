fe26b262649dcc274dc54206868f1e3349d3fd52
Fatoki Lawrence <93355867+jydestudio@users.noreply.github.com>
2025-03-24 15:02:18 +0100

    Update app.js

--- futaWebMap-main/app.js
+++ futaWebMap-main/app.js
@@ -2,20 +2,19 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 // const cors = require("cors");
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI("AIzaSyAZLHFiKbHpPdBhDQNVWbmNnKdLy1NoPoc");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 // app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/ask-ai", async (req, res) => {
     const userQuestion = req.query.message.toLowerCase();  // Get the question from frontend
